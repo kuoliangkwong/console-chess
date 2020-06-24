4a22549658e96cff38f75ca3b8b6328d519763b9
kuoliangkwong <kuoliang.kwong@gmail.com>
2020-06-24 13:48:48 +0800

    added pawn attack rule

--- Models/Pawn.cs
+++ Models/Pawn.cs
@@ -6,8 +6,28 @@ public class Pawn: ChessPiece
 {
     public Pawn(Color color) : base(color)
     {
-        moveRules.Add(new ForwardMoveRule());
+        moveRules.Add(new OrGroupRules(
+            new PawnAttack(),
+            new ForwardMoveRule()
+        ));
         moveRules.Add(new CheckBlockingRule());
+    }
+
+    class PawnAttack : IMoveRule
+    {
+        public bool IsValid(Vector2Int src, Vector2Int dst, Board board)
+        {
+            var srcPiece = board.Occupants.SafeGetValue(src);
+            var dstPiece = board.Occupants.SafeGetValue(dst);
+
+            if (dstPiece == null || srcPiece == null) return false;
+            if (srcPiece.Color == dstPiece.Color) return false;
+
+            var vector = Vector2Int.Substract(dst, src);
+
+            return (src.Y + srcPiece.Forward == dst.Y) &&
+                   (vector.Abs().X == 1);
+        }
     }
 }
 
