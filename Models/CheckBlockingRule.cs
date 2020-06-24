60ad5ba58cb6a507a8228353ca5cbfeb21cbacdd
kuoliangkwong <kuoliang.kwong@gmail.com>
2020-06-24 12:07:28 +0800

    added blocking checking rule

---
+++ Models/CheckBlockingRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class CheckBlockingRule : IMoveRule
+{
+    public bool IsValid(Vector2Int src, Vector2Int dst, Board board)
+    {
+        var vector = Vector2Int.Substract(dst, src);
+        var signVector = vector.SignVector;
+        var tempVector = new Vector2Int(signVector);
+        while (!tempVector.Equal(vector))
+        {
+            var piece = board.Occupants.SafeGetValue(
+                src.X + tempVector.X,
+                src.Y + tempVector.Y
+            );
+            if (piece != null) return false;
+            tempVector.Add(signVector);
+        }
+        return true;
+    }
+}
+
