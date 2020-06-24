c7fffde8d25468f7ba74ce944f08cd160c170f2a
kuoliangkwong <kuoliang.kwong@gmail.com>
2020-06-24 12:08:48 +0800

    added King and Pawn

---
+++ Models/King.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class King : ChessPiece
+{
+    public King(Color color) : base(color) 
+    {
+        moveRules.Add(
+            new OrGroupRules(
+                new DiagonalMoveRule(1),
+                new StraightMoveRule(1)
+            )
+        );
+        moveRules.Add(new CheckBlockingRule());
+    }
+
+}
+
---
+++ Models/Pawn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Pawn: ChessPiece
+{
+    public Pawn(Color color) : base(color)
+    {
+        moveRules.Add(new ForwardMoveRule());
+        moveRules.Add(new CheckBlockingRule());
+    }
+}
+
