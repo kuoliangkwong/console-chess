ed840d8337099ae62a2b20e64569cf0d8dc9b26a
kuoliangkwong <kuoliang.kwong@gmail.com>
2020-06-24 12:08:06 +0800

    added XOR combination for move rules

---
+++ Models/OrGroupRules.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class OrGroupRules : IMoveRule
+{
+    readonly IMoveRule[] rules;
+
+    public OrGroupRules(params IMoveRule[] rules)
+    {
+        this.rules = rules;
+    }
+
+    public bool IsValid(Vector2Int src, Vector2Int dst, Board board)
+    {
+        var valid = false;
+        foreach(var rule in rules)
+        {
+            valid = valid || rule.IsValid(src, dst, board);
+        }
+        return valid;
+    }
+}
+
--- Models/Queen.cs
+++ Models/Queen.cs
@@ -7,7 +7,13 @@ public class Queen : ChessPiece
 {
     public Queen(Color color) : base(color) 
     {
-        
+        moveRules.Add(
+            new OrGroupRules(
+                new DiagonalMoveRule(),
+                new StraightMoveRule()
+            )
+        );
+        moveRules.Add(new CheckBlockingRule());
     }
 
 }
